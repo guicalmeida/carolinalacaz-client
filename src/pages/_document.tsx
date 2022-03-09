ed3ae927821539dc847eb33c14bd2bd6bb1d9710
Guilherme Almeida <guica.almeida@gmail.com>
2022-03-08 21:29:07 -0300

    fix: next upgrade

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,10 +37,17 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
+          <script src="https://code.iconify.design/1/1.0.7/iconify.min.js" />
         </body>
       </Html>
     )
